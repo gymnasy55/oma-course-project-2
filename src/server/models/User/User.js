6401e4c2d61b34389cff1a1db4e994a36ed17dbf
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-04-25 22:22:46 +0300

    small refactoring

---
+++ src/server/models/User/User.js
@@ -0,0 +1,9 @@
+class User {
+  constructor(id, login, password) {
+    this.id = id
+    this.login = login
+    this.password = password
+  }
+}
+
+export { User }
---
+++ src/server/service/ServerOptions.js
@@ -0,0 +1,8 @@
+class ServerOptions {
+  constructor(method, url) {
+    this.method = method.toUpperCase()
+    this.url = url
+  }
+}
+
+export { ServerOptions }
