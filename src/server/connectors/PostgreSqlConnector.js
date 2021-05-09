b7c43a6df5a7888422cc682354e7a1bc8565d54a
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-05-10 00:46:54 +0300

    refactor of BaseConnector.js

--- src/server/connectors/PostgreSqlConnector.js
+++ src/server/connectors/PostgreSqlConnector.js
@@ -36,37 +36,37 @@ class PostgreSqlConnector extends BaseConnector {
   }
 
   getAllPersons(func) {
-    super.getAllPersons(func)
+    super.getAllPersons()
     this.#query('SELECT * FROM persons', func)
   }
 
   getPersons(func) {
-    super.getPersons(func)
+    super.getPersons()
     this.#query('SELECT * FROM persons WHERE deleted=0', func)
   }
 
   getPersonsByUserId(userId, func) {
-    super.getPersonsByUserId(userId, func)
+    super.getPersonsByUserId(userId)
     this.#query(`SELECT * FROM persons WHERE user_id=${userId} AND deleted=0`, func)
   }
 
   getDeletedPersonsByUserId(userId, func) {
-    super.getDeletedPersonsByUserId(userId, func)
+    super.getDeletedPersonsByUserId(userId)
     this.#query(`SELECT * FROM persons WHERE user_id=${userId} AND deleted=1`, func)
   }
 
   postPerson(person, func) {
-    super.postPerson(person, func)
+    super.postPerson(person)
     this.#query(`INSERT INTO persons VALUES (DEFAULT, '${person.fname}', '${person.lname}', ${person.age}, '${person.city}', '${person.phoneNumber}', '${person.email}', '${person.companyName}', '${person.userId}', 0)`, func)
   }
 
   putPerson(person, func) {
-    super.putPerson(person, func)
+    super.putPerson(person)
     this.#query(`UPDATE persons SET fname='${person.fname}', lname='${person.lname}', age=${person.age}, city='${person.city}', phoneNumber='${person.phoneNumber}', email='${person.email}', companyName='${person.companyName}' WHERE id=${person.id}`, func)
   }
 
   deletePersonById(personId, func) {
-    super.deletePersonById(personId, func)
+    super.deletePersonById(personId)
     this.#query(`UPDATE persons SET deleted=1 WHERE id=${personId}`, func)
   }
 }
